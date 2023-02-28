b8484c77f87a26468b531a85276a41264a28b851
Ondeyo Matoke <ondeyostephen30@gmail.com>
2023-02-28 21:49:05 +0300

    additions

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 import SideBar from './Components/SideBar/SideBar';
 
+
 function App() {
   return (
     <div className='Application'>
--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function SideBar() {
   return (
     <div>
-      
+      <nav>
+        <ul>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+
+            <li>
+                <Link to="chatgpt">ChatGpt</Link>
+            </li>
+
+            <li>
+                <Link to="pressbooks">Pressbooks</Link>
+            </li>
+
+            <li>
+                <Link to="oercommons">Oercommons</Link>
+            </li>
+
+            <li>
+                <Link to="creativecommons">Creativecommons</Link>
+            </li>
+
+        </ul>
+      </nav>
     </div>
   )
 }
